3df5d965ce2da21f52d117062ab470bedaf3ae44
raihanalbazzy <raihan.bazzy@gotocompany.com>
2024-01-30 09:26:15 +0700

    chore: update session, add tanstack dev

--- src/lib/Context/AuthProvider/index.tsx
+++ src/lib/Context/AuthProvider/index.tsx
@@ -1,9 +1,3 @@
 "use client";
-import React, { PropsWithChildren } from "react";
-import { SessionProvider } from "next-auth/react";
-
-const AuthProvider = ({ children }: PropsWithChildren) => {
-  return <SessionProvider>{children}</SessionProvider>;
-};
-
+import { SessionProvider as AuthProvider } from "next-auth/react";
 export default AuthProvider;
--- src/lib/Context/TanstackProvider/index.tsx
+++ src/lib/Context/TanstackProvider/index.tsx
@@ -2,6 +2,7 @@
 
 import { PropsWithChildren, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function TanstackProvider({ children }: PropsWithChildren) {
   const [queryClient] = useState(
@@ -16,6 +17,9 @@ export default function TanstackProvider({ children }: PropsWithChildren) {
   );
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
